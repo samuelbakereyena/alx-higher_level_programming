6611b0bd27ccd39ac4f9e82e6bc9e59e39fb869f
Eyena Samuel Baker <samuelbaker.ey.com>
2021-11-19 05:15:44 +1000

    positve or negative values

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+number =input()
+number = int(number)
+if number > 0:
+    print("{} is positive".format(number))
+elif number ==0:
+    print("{} is zero".format(number))
+elif number < 0:
+    print("{} is negative".format(number))
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,2 @@
+README in this root folder describe the use of if/else conditons
+
