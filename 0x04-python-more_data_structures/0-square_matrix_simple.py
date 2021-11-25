2dcbbff1269d234e06a66401333947090ba6d7ce
Eyena Samuel Baker <samuelbaker.ey.com>
2021-11-25 14:59:56 +0300

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-   if matrix is None:
-       return
-   new_matrix =[[n * n for n in a] for a in matrix]
-   return new_matrix
-
+    if matrix is None:
+        return
+    new_matrix =[[n * n for n in a] for a in matrix]
+    return new_matrix
