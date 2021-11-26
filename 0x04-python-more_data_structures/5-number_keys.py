b01f1a8b7ee51450609f90996ef1dcca998c780b
Eyena Samuel Baker <samuelbaker.ey.com>
2021-11-26 08:17:01 +0300

    counting number of dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    if a_dictionary is None:
+        return 0
+    return (len(a_dictionary))
+    #number_keys = 0
+    #for i in enumerate(dict):
+   #     number_keys += 1
+    #    return count
+    #print(number_keys(a_dictionary))
